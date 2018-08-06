e61b737a089c7eca7b3de2ed2d2e4ccc9a54b9d1
Kathy Daniels <danielskathyd@gmail.com>
2018-08-05 20:49:26 -0700

    Event handlers

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const morgan = require('morgan');
 let app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(morgan('dev'));
+
 app.get('/', function(req, res) {
-  console.log('Hello');
+  console.log('Jeffrey');
   res.send('Hello World!');
+});
+
+app.get('/page/:slug', function(req, res) {
+  console.log(req.params.slug);
+  let pageData = {
+    title: 'Email Forwarding',
+    slug: 'email-forwarding',
+    contributors: ['Jeffrey Chan', 'Kathy Daniels'],
+    content:
+      '<h1>EMAIL FORWARDING AND ALIASING</h1> <p>Written by Jeffrey Chan</p>'
+  };
+  res.json(pageData);
 });
 
 app.listen(app.get('port'), function() {
