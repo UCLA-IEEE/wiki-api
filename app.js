0b2eb37ab9370f6a8778933ea445d49731f62f7b
Jeffrey Chan <jeffschan97@gmail.com>
2018-08-13 21:12:35 -0700

    Added cors

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const express = require('express');
-const morgan = require('morgan');
-let app = express();
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 
-app.set('port', process.env.PORT || 3000);
+let app = express()
 
-app.use(morgan('dev'));
+app.set('port', process.env.PORT || 3000)
+app.use(morgan('dev'))
+app.use(cors())
 
 app.get('/', function(req, res) {
-  console.log('Jeffrey');
-  res.send('Hello World!');
-});
+  console.log('Jeffrey')
+  res.send('Hello World!')
+})
 
 app.get('/page/:slug', function(req, res) {
-  console.log(req.params.slug);
+  console.log(req.params.slug)
   let pageData = {
     title: 'Email Forwarding',
     slug: 'email-forwarding',
     contributors: ['Jeffrey Chan', 'Kathy Daniels'],
-    content:
-      '<h1>EMAIL FORWARDING AND ALIASING</h1> <p>Written by Jeffrey Chan</p>'
-  };
-  res.json(pageData);
-});
+    content: '<h1>EMAIL FORWARDING AND ALIASING</h1> <p>Written by Jeffrey Chan</p>'
+  }
+  res.json(pageData)
+})
 
 app.listen(app.get('port'), function() {
-  console.log('Started server at port 3000.');
-});
+  console.log('Started server at port 3000.')
+})
