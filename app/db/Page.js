fec228acf997a717c96741149193ed98e2027d45
Jeffrey Chan <jeffschan97@gmail.com>
2018-10-13 01:36:32 -0700

    Added timestamps to pages

--- app/db/Page.js
+++ app/db/Page.js
@@ -10,17 +10,20 @@ const Comment = new mongoose.Schema({
 })
 
 // Page defines the schema for a page
-const Page = new mongoose.Schema({
-  title: { type: String, required: true, minLength: 1 },
-  slug: { type: String, required: true, minLength: 1 },
+const Page = new mongoose.Schema(
+  {
+    title: { type: String, required: true, minLength: 1 },
+    slug: { type: String, required: true, minLength: 1 },
 
-  contributors: { type: [String], default: [] },
+    contributors: { type: [String], default: [] },
 
-  tags: { type: [String], default: [] },
+    tags: { type: [String], default: [] },
 
-  comments: { type: [Comment], default: [] },
+    comments: { type: [Comment], default: [] },
 
-  content: { type: String, required: true, minLength: 1 }
-})
+    content: { type: String, required: true, minLength: 1 }
+  },
+  { timestamps: true }
+)
 
 module.exports = mongoose.model(collectionName, Page)
